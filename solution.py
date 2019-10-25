83ee463f7d953201afe93182e2519c46d3ad639b
e <3740993+ecterceocgan@users.noreply.github.com>
2019-10-24 21:12:32 -0400

    Add function to read trip data (#4)

--- solution.py
+++ solution.py
@@ -11,28 +11,30 @@ pixel_per_km = 5
 avg_speed_per_min = 1
 vehicle_capacity = 3
 
-# Read in data as Pandas DataFrame
-data = pd.read_csv('Simpsons.txt', sep='\t', skiprows=1, header=None,
-                   names=['Requester', 'Trip #', 'Depart After', 'Arrive Before',
-                          'X1', 'Y1', 'X2', 'Y2'],
-                   parse_dates=['Depart After', 'Arrive Before'])
 
-# Convert Departure/Arrival times to minutes after earliest stop (8:00am)
-data.sort_values(['Depart After', 'Arrive Before'], inplace=True)
-time_zero = data['Depart After'].iloc[0]
-data['Depart After'] = data['Depart After'].apply(lambda x: (x-time_zero).total_seconds()/60)
-data['Arrive Before'] = data['Arrive Before'].apply(lambda x: (x-time_zero).total_seconds()/60)
+def read_trip_data(filename):
+    data = pd.read_csv('Simpsons.txt', sep='\t', skiprows=1, header=None,
+                       names=['requester', 'trip_id', 'depart_after', 'arrive_before',
+                              'X1', 'Y1', 'X2', 'Y2'],
+                       parse_dates=['depart_after', 'arrive_before'])
 
-data['Origin Loc.'] = list(np.array(zip(data['X1'], data['Y1'])))
-data['Dest. Loc.'] = list(np.array(zip(data['X2'], data['Y2'])))
-data.drop(['X1', 'Y1', 'X2', 'Y2'], inplace=True, axis=1)
+    # Convert departure/arrival times to number of minutes after earliest stop
+    data.sort_values(['depart_after', 'arrive_before'], inplace=True)
+    time_zero = data['depart_after'].iloc[0]
+    data['depart_after'] = (data['depart_after'] - time_zero).dt.total_seconds() / 60
+    data['arrive_before'] = (data['arrive_before'] - time_zero).dt.total_seconds() / 60
+
+    data['origin_loc'] = list(np.array(zip(data['X1'], data['Y1'])))
+    data['destination_loc'] = list(np.array(zip(data['X2'], data['Y2'])))
+    data.drop(['X1', 'Y1', 'X2', 'Y2'], inplace=True, axis=1)
+    return data, time_zero
 
 
 def recursive_routes(node, trip_data, trip_id_time_index, time_matrix, leaves, connect_to=None):
     """Recursively create tree of possible routes while ensuring vehicle isn't full."""
     for child_trip_id, child_pickup in node.route_options.iteritems():
         # Get name of requester
-        requester = trip_data.loc[trip_data['Trip #'] == child_trip_id, 'Requester'].item()
+        requester = trip_data.loc[trip_data['trip_id'] == child_trip_id, 'requester'].item()
 
         # Create child route node based on whether they need to be picked up or dropped off
         if (child_pickup is True) and (node.num_in_car < vehicle_capacity):
@@ -53,18 +55,18 @@ def recursive_routes(node, trip_data, trip_id_time_index, time_matrix, leaves, c
 
             # Updating trip time by connecting to end of route in previous block (if applicable)
             if connect_to is not None:
-                loc1 = data.loc[data['Trip #'] == connect_to.trip_id, 'Dest. Loc.'].item()
-                loc2 = data.loc[data['Trip #'] == child_trip_id, 'Origin Loc.'].item()
+                loc1 = data.loc[data['trip_id'] == connect_to.trip_id, 'destination_loc'].item()
+                loc2 = data.loc[data['trip_id'] == child_trip_id, 'origin_loc'].item()
                 trip_time = np.linalg.norm(loc1-loc2)/pixel_per_km/avg_speed_per_min
 
             # Checking if it's too early to be picked up; if so, must wait
             depart_after_time = (
-                trip_data.loc[trip_data['Trip #'] == child_trip_id, 'Depart After'].item())
+                trip_data.loc[trip_data['trip_id'] == child_trip_id, 'depart_after'].item())
             if (node.total_time + trip_time) < depart_after_time:
                 trip_time = depart_after_time - node.total_time
 
             child_node_name = 'Pickup %i:%s' % (child_trip_id, requester)
-            child_loc = trip_data.loc[trip_data['Trip #'] == child_trip_id, 'Origin Loc.'].item()
+            child_loc = trip_data.loc[trip_data['trip_id'] == child_trip_id, 'origin_loc'].item()
             child_route_node = RouteNode(child_node_name, child_trip_id, child_pickup, child_loc,
                                          child_route_options, trip_time, parent=node)
         elif child_pickup is False:
@@ -84,7 +86,7 @@ def recursive_routes(node, trip_data, trip_id_time_index, time_matrix, leaves, c
             trip_time = time_matrix[parent_loc_id, child_loc_id]
 
             child_node_name = 'Dropoff %i:%s' % (child_trip_id, requester)
-            child_loc = trip_data.loc[trip_data['Trip #'] == child_trip_id, 'Dest. Loc.'].item()
+            child_loc = trip_data.loc[trip_data['trip_id'] == child_trip_id, 'destination_loc'].item()
             child_route_node = RouteNode(child_node_name, child_trip_id, child_pickup, child_loc,  # noqa
                                          child_route_options, trip_time, parent=node)  # noqa
         else:
@@ -110,12 +112,12 @@ def check_if_late(node, trip_data):
     for trip_id, pickup in node.route_options.iteritems():
         if trip_id == node.trip_id:  # check if self is late
             arrive_before_time = (
-                trip_data.loc[trip_data['Trip #'] == node.trip_id, 'Arrive Before'].item())
+                trip_data.loc[trip_data['trip_id'] == node.trip_id, 'arrive_before'].item())
             if node.total_time > arrive_before_time:  # late!
                 return True
         else:  # check if others (who have been picked up) are late
             arrive_before_time = (
-                trip_data.loc[trip_data['Trip #'] == trip_id, 'Arrive Before'].item())
+                trip_data.loc[trip_data['trip_id'] == trip_id, 'arrive_before'].item())
             if pickup is False and node.total_time > arrive_before_time:  # late!:
                 return True
     return False
@@ -127,15 +129,15 @@ def assign_vehicles(route_leaves, vehicles, previous_vehicles):
     Recursively find routes for trips left uncompleted and assign to new vehicle.
     """
     # Sort routes by length and time taken to complete
-    routes = pd.DataFrame(columns=['Last Node', 'Num. Stops', 'Total Time'])
+    routes = pd.DataFrame(columns=['last_node', 'num_stops', 'total_time'])
     for l in route_leaves:
         routes = routes.append(pd.DataFrame([[l, len(l.ancestors), l.total_time]],
-                               columns=['Last Node', 'Num. Stops', 'Total Time']))
-    sorted_routes = (routes.sort_values(['Num. Stops', 'Total Time'], ascending=[False, True])
+                               columns=['last_node', 'num_stops', 'total_time']))
+    sorted_routes = (routes.sort_values(['num_stops', 'total_time'], ascending=[False, True])
                      .reset_index(drop=True))
 
     # Select route with shortest time and most trips completed as 'best'
-    longest_route = sorted_routes.iloc[0]['Last Node']
+    longest_route = sorted_routes.iloc[0]['last_node']
     vehicles.append(longest_route)
 
     # Check for remaining trips, recurse and assign to new vehicle
@@ -168,11 +170,12 @@ def itinerary(vehicles_full_trips):
     return itinerary_str
 
 
+data, time_zero = read_trip_data('Simpsons.txt')
 # Exploit information about there being only 5 customers and attempt to find
 # "blocks" of rides that have to be completed before the next "block" starts
 blocks = [0]  # array holding indices of block start/stops
 for i in xrange(len(data)-1):
-    if (data.iloc[i]['Arrive Before'] < data.iloc[i+1]['Depart After']):
+    if (data.iloc[i]['arrive_before'] < data.iloc[i+1]['depart_after']):
         blocks.append(i+1)
 blocks.append(len(data))
 
@@ -182,8 +185,8 @@ vehicles_routes = [[] for b in xrange(len(blocks)-1)]
 for b in xrange(len(blocks)-1):
     # Calculate distances between all origin/destination locations within a block
     block_data = data.iloc[blocks[b]:blocks[b+1]]
-    origin_loc = list(block_data['Origin Loc.'])
-    dest_loc = list(block_data['Dest. Loc.'])
+    origin_loc = list(block_data['origin_loc'])
+    dest_loc = list(block_data['destination_loc'])
     all_loc = origin_loc + dest_loc
     dist_matrix = np.zeros((2 * len(block_data), 2 * len(block_data)))
     for i, i_loc in enumerate(all_loc):
@@ -192,8 +195,8 @@ for b in xrange(len(blocks)-1):
     time_matrix = dist_matrix/avg_speed_per_min
 
     # Define possible route starts within a block
-    route_options = {block_data.iloc[i]['Trip #']: True for i in xrange(len(block_data))}
-    trip_id_time_index = {block_data.iloc[i]['Trip #']: i for i in xrange(len(block_data))}
+    route_options = {block_data.iloc[i]['trip_id']: True for i in xrange(len(block_data))}
+    trip_id_time_index = {block_data.iloc[i]['trip_id']: i for i in xrange(len(block_data))}
     root_route = RouteNode(name='start', route_opt=route_options)
 
     block_leaves = []
